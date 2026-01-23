914ab1cc80c06847b2b921f309129fc0c1d08cd7
hashmiarsa <hashmiayan90@gmail.com>
2026-01-23 22:56:15 +0530

    change

--- hashmiayan90-portfolio-main/vite.config.ts
+++ hashmiayan90-portfolio-main/vite.config.ts
@@ -1,3 +1,24 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+// import { componentTagger } from "lovable-tagger";
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//     hmr: {
+//       overlay: false,
+//     },
+//   },
+//   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -5,6 +26,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/myportfolio/", // Your repo name
   server: {
     host: "::",
     port: 8080,
